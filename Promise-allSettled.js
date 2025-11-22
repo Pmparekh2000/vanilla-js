c765111a47d75a63386e054d11a3265eb9b35386
pmparekh <prerak.parekh@nielsen.com>
2025-11-22 15:01:24 +0530

    Promise-allSettled polyfill

--- Promise-allSettled.js
+++ Promise-allSettled.js
@@ -1,7 +1,9 @@
-Promise.myAll = function (promises) {
+Promise.myAllSettled = function (promises) {
   function executor(resolve, reject) {
     if (!Array.isArray(promises)) {
-      reject(new TypeError("Promise.myAll requires an array or iterable"));
+      reject(
+        new TypeError("Promise.myAllSettled requires an array or iterable")
+      );
       // Adding return to prevent any further synchronous piece of code to get executed
       // and ending the program here itself.
       return;
@@ -14,30 +16,34 @@ Promise.myAll = function (promises) {
       return;
     }
     const results = new Array(promises.length);
-    let resolvedCount = 0;
+    let settledCount = 0;
     promises.forEach((item, index) => {
       // Handling non-Promise values by wrapping them in Promise.resolve
       // This ensures all items are treated as Promises, correctly adopting their value.
       const promise = item instanceof Promise ? item : Promise.resolve(item);
+
       promise
         .then((res) => {
           // Store the result at the original index
-          results[index] = res;
-          resolvedCount++;
-
-          // Check if all promises have resolved
-          if (resolvedCount === promises.length) {
-            // Not adding return here since .then block is async piece of code.
+          results[index] = { status: "fullfilled", value: res };
+          settledCount++;
+          // Check that if the obtained results length is equal to promises length
+          if (settledCount === promises.length) {
             resolve(results);
           }
         })
         .catch((error) => {
-          // Reject immediately on the first rejection
-          // Not adding return here since .catch block is async piece of code.
-          reject(error);
+          // Store the error at the original index
+          results[index] = { status: "rejected", reason: error };
+          settledCount++;
+          // Check that if the obtained results length is equal to promises length
+          if (settledCount === promises.length) {
+            resolve(results);
+          }
         });
     });
   }
+
   return new Promise(executor);
 };
 
@@ -57,15 +63,18 @@ const pr3 = new Promise((resolve, reject) => {
   }, 3000);
 });
 
-// const pr4 = new Promise((resolve, reject) => {
-//   console.log(4);
-//   setTimeout(() => {
-//     reject(44);
-//   }, 10000);
-// });
+const pr4 = new Promise((resolve, reject) => {
+  console.log(4);
+  setTimeout(() => {
+    reject(44);
+  }, 5000);
+});
 
-const promises = [pr1, pr2, pr3];
-Promise.myAll(promises)
+// The most important distinction of Promise.allSettled (compared to Promise.all) is that
+// it always resolves with an array of objects, where each object explicitly states the status
+// ("fulfilled" or "rejected") and contains either the value or the reason for the same.
+const promises = [pr1, pr2, pr3, pr4];
+Promise.myAllSettled(promises)
   .then((res) => {
     console.log(res);
   })
